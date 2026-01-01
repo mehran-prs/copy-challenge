4376a6fd10d4cf8ea934b6eb6e93ebd6d19464db
mehran.pourasdeqi <mehr.prs@gmail.com>
2026-01-02 00:28:27 +0330

    chore: fix aspect ratrio

--- sw.js
+++ sw.js
@@ -1,20 +1,33 @@
-const CACHE_NAME = 'pen-matcher-v1';
+const CACHE_NAME = 'pen-matcher-v2';
 const ASSETS = [
     './',
     './index.html',
     './manifest.json'
 ];
 
-// Install Service Worker
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
-// Fetch Assets from Cache
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+        ).then(() => self.clients.claim())
+    );
+});
+
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request))
+        fetch(e.request)
+            .then((res) => {
+                const clone = res.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+                return res;
+            })
+            .catch(() => caches.match(e.request))
     );
 });
